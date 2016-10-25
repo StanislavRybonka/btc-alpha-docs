44bab8ff71f868b6d83f194659ec110dc810e803
StanislavRybonka <stanislav1991r@gmail.com>
2016-10-25 18:05:52 +0300

    Changed name of project.

--- source/conf.py
+++ source/conf.py
@@ -49,7 +49,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'btc-alpha'
+project = 'BTC-Alpha'
 copyright = '2016, Vitaliy Bodnar'
 author = 'Vitaliy Bodnar'
 
